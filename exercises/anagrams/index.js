734fa32b685739c97545398bbbf753fa56e89119
nataliia-v <nataly.verbenskaya@gmail.com>
2019-12-19 17:21:40 +0200

    anagram solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,13 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return clearString(stringA) === (clearString(stringB))
+}
+
+function clearString (str) {
+
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;

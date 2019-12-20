b6033f71fe0bacf9eb3a575c31642761d4ef85d2
nataliia-v <nataly.verbenskaya@gmail.com>
2019-12-20 14:58:38 +0200

    vowels #2 solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,15 +8,21 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-  let count = 0;
-  const checker = ['a', 'e', 'i', 'o', 'u' ];
+  //#2 solution
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0
 
-  for (let char of str.toLowerCase()){
-    if (checker.includes(char)){
-      count++
-    }
-  }
-  return count;
 }
+
+// //#1 solution
+// let count = 0;
+// const checker = ['a', 'e', 'i', 'o', 'u' ];
+//
+// for (let char of str.toLowerCase()){
+//   if (checker.includes(char)){
+//     count++
+//   }
+// }
+// return count;
 
 module.exports = vowels;
